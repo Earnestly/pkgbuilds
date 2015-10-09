0dce6a179b3dedb0e0c974b7fd8238b4468e097d
Earnestly <zibeon@gmail.com>
2015-10-09 01:17:52 +0100

    util.bash: Document the surprising behaviour of git fetch in a bare mirror.

--- util.bash
+++ util.bash
@@ -1,20 +1,23 @@
-# Usage: in_array object array
-#        object = item to search for
-#        array  = array in which to look
+# Usage: in_array article array
+#        mark  : item to search for
+#        array : array in which to look
 in_array() {
-	local object=$1
-	local seeker
+	local mark=$1
+	local element
 
-	for seeker in "${@:2}"; do
-		[[ $seeker == "$object" ]] && return 0
+	for element in "${@:2}"; do
+		if [[ $element == "$mark" ]]; then
+            return 0
+        fi
 	done
-
+    
+    # Mark not found.
 	return 1
 }
 
 # Usage: confirm bias format ...
-#        bias   = yes | no
-#        format = printf formatter
+#        bias   : yes | no
+#        format : printf formatter
 confirm() {
     local -l reply
     local message prompt
@@ -34,11 +37,21 @@ confirm() {
     esac
 }
 
-# Comment: This is specific to fetching changes for bare mirrors.
+# Comment: This procedure is specific to fetching changes for bare mirrors.
 # Usage: check_updates_git branch
 check_updates_git() {
-    local branch="$1"
+    local branch=$1
+    local format='%C(auto)%h %C(blue)%an %C(green bold)(%cr) %C(reset)%s'
 
+    # Using symbolic-ref is required as bare repos have no checkout, without
+    # this git will do the wrong thing and update the HEAD regardless of what
+    # the refs point at even if git is explicitly configured to not do this.
+
+    # This was changed because users would find it "confusing" if this they
+    # couldn't compare the HEAD against the branch they explicitly fetched from
+    # and didn't realise they could use FETCH_HEAD instead.
+
+    # From the school of DWIM: https://github.com/git/git/commit/f269048754f3
     git symbolic-ref HEAD refs/heads/"$branch"
 
     git fetch "$(git config remote.origin.url)" "$branch" 2> /dev/null
@@ -47,9 +60,11 @@ check_updates_git() {
 
     if ((distance > 0)); then
         if confirm yes '%d new commits found, view log?' "$distance"; then
-            git log --pretty=format:'%C(auto)%h %C(blue)%an %C(green bold)(%cr) %C(reset)%s' HEAD...FETCH_HEAD
+            git log --pretty=format:"$format" HEAD...FETCH_HEAD
             return 0
         fi
     fi
+
+    # No new commits found.
     return 1
 }
